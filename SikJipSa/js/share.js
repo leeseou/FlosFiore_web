536f638d9f349f5eb8902c16ebcc377df63a9760
KimHyeyeonn <85014114+KimHyeyeonn@users.noreply.github.com>
2022-08-24 19:50:34 +0900

    Update share.js

--- SikJipSa/js/share.js
+++ SikJipSa/js/share.js
@@ -1,5 +1,22 @@
 const url = 'https://sikjipsa.netlify.app/';
 
+const share = document.querySelector("#share");
+
+function sharebt(){
+  result.style.WebkitAnimation = "fadeOut 1s";
+  result.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    share.style.WebkitAnimation = "fadeIn 1s";
+    share.style.animation = "fadeIn 1s";
+    setTimeout(() => {
+      result.style.display = "none";
+      share.style.display = "block"
+    }, 450)
+    let qIdx = 0;
+    goNext(qIdx);
+  }, 450);
+}
+
 function setShare(){
   var resultImg = document.querySelector('#resultImg');
   var resultAlt = resultImg.firstElementChild.alt;
