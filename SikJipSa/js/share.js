6d546f39663577e5147e4f9723f28f7bea382e8f
eunpse <69415018+eunpse@users.noreply.github.com>
2022-08-24 20:27:29 +0900

    Add files via upload

--- SikJipSa/js/share.js
+++ SikJipSa/js/share.js
@@ -2,21 +2,6 @@ const url = 'https://sikjipsa.netlify.app/';
 
 const share = document.querySelector("#share");
 
-function sharebt(){
-  result.style.WebkitAnimation = "fadeOut 1s";
-  result.style.animation = "fadeOut 1s";
-  setTimeout(() => {
-    share.style.WebkitAnimation = "fadeIn 1s";
-    share.style.animation = "fadeIn 1s";
-    setTimeout(() => {
-      result.style.display = "none";
-      share.style.display = "block"
-    }, 450)
-    let qIdx = 0;
-    goNext(qIdx);
-  }, 450);
-}
-
 function setShare(){
   var resultImg = document.querySelector('#resultImg');
   var resultAlt = resultImg.firstElementChild.alt;
@@ -25,26 +10,4 @@ function setShare(){
   const shareImage = url + 'img/image-' + resultAlt + '.png';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
 
-  Kakao.Link.sendDefault({
-    objectType: 'feed',
-    content: {
-      title: shareTitle,
-      description: shareDes,
-      imageUrl: shareImage,
-      link: {
-        mobileWebUrl: shareURL,
-        webUrl: shareURL
-      },
-    },
-
-    buttons: [
-      {
-        title: '결과확인하기',
-        link: {
-          mobileWebUrl: shareURL,
-          webUrl: shareURL,
-        },
-      },
-    ]
-  });
 }
